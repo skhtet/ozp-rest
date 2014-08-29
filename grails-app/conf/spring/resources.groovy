b12b4de3c61274ce4fe15684c5b49e62dec48209
Rob Szewczyk <rob@gmail.com>
2014-08-29 14:32:56 -0400

    More deletions and cleanup

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -49,16 +49,6 @@ beans = {
                     autoRoles = [Constants.USER]
                 }
                 break
-            case "testAdmin1":
-                println("Using AutoLoginAccountService - you will be logged in as testAdmin1")
-                accountService(AutoLoginAccountService) {
-                    autoAccountUsername = "testAdmin1"
-                    autoAccountName = "Test Administrator 1"
-                    autoAccountEmail = "testadmin1@nowhere.com"
-                    autoOrganization = DEFAULT_AGENCY
-                    autoRoles = [Constants.USER, Constants.ADMIN, Constants.EXTERNADMIN]
-                }
-                break
             default:
                 if (GrailsUtil.environment == "test" || GrailsUtil.environment.startsWith('with_')) {
                     println("Using AutoLoginAccountService - you will be logged in as testUser1")
@@ -69,8 +59,14 @@ beans = {
                         autoRoles = [Constants.USER]
                     }
                 } else {
-                    println("Not using AutoLoginAccountService - if you want to do so, set -Duser=[testUser1|testAdmin1|testAdmin2] in your environment")
-                    accountService(AccountService)
+                    println("Using AutoLoginAccountService - you will be logged in as testAdmin1")
+                    accountService(AutoLoginAccountService) {
+                        autoAccountUsername = "testAdmin1"
+                        autoAccountName = "Test Administrator 1"
+                        autoAccountEmail = "testadmin1@nowhere.com"
+                        autoOrganization = DEFAULT_AGENCY
+                        autoRoles = [Constants.USER, Constants.ADMIN, Constants.EXTERNADMIN]
+                    }
                 }
                 break
         }
@@ -80,57 +76,6 @@ beans = {
     }
 
     customPropertyEditorRegistrar(util.CustomPropertyEditorRegistrar)
-
-	mp_RESTInterceptorService(ozone.interceptor.MP_RESTInterceptor) {}
-
-	mp_BannerBeanService(ozone.banner.MP_BannerBean) {
-		enableBanner = false
-		header = "<div class='north-banner-text'>Welcome to Marketplace</div>";
-		footer = "<div class='south-banner-text'>Welcome to Marketplace</div>";
-		css = """ body {
-					padding-top: 100px!important;
-					padding-bottom: 22px!important;
-				}
-
-				#header {
-					top: 19px!important;
-				}
-
-				#footer {
-					bottom: 19px!important;
-				}
-
-				#TypeMenuId, #DomainMenuId, #CategoryMenuId, #AgencyMenuId {
-				  z-index: 18000 !important;
-				  top: 155px !important;
-				}
-
-				#northBanner, #southBanner {
-					display: block;
-				}
-				.bannerClass .north-banner-text {
-					color: blue;
-					text-align: center;
-					font-size: 17px;
-					background-color: orange;
-					font-weight: bold;
-				}
-				.bannerClass .south-banner-text {
-					color: pink;
-					text-align: center;
-					font-size: 17px;
-					background-color: green;
-					font-weight: bold;
-				}""";
-        js = """jQuery('html').addClass('bannerClass');""";
-    }
-
-    OzoneConfiguration(ozone3.Configuration) {
-
-        // log4j file watch interval in milliseconds
-        log4jWatchTime = 180000; // 3 minutes
-        freeTextEntryWarningMessage = ""
-    }
 
     /**
      * OP-5818: Use the Spring class instead of the default Grails subclass of it.
