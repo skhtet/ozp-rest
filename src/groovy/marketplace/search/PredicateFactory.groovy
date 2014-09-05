d16085aaa06fa07f47e73617b93376562746d54a
Nish Patel <nishantpatel611@gmail.com>
2014-09-05 16:20:59 -0400

    implement isFeatured option

--- src/groovy/marketplace/search/PredicateFactory.groovy
+++ src/groovy/marketplace/search/PredicateFactory.groovy
@@ -38,7 +38,8 @@ class PredicateFactory implements Serializable {
                 owfWidgetType: new SingleValuePredicate(predicateName: "owfWidgetType", indexFieldName: "owfProperties.owfWidgetType", paramName: "owfWidgetType"),
                 type: new ExactValuePredicate(predicateName: "types", indexFieldName: "types.title", paramName: "type"),
                 category: new MultiExactValuePredicate(predicateName: "type", indexFieldName: "categories.title", paramName: "category"),
-                agency: new MultiValuePredicate(predicateName: "type", indexFieldName: "agency.title", paramName: "agency")
+                agency: new MultiValuePredicate(predicateName: "type", indexFieldName: "agency.title", paramName: "agency"),
+                isFeatured: new SingleValuePredicate(predicateName: "isFeatured", indexFieldName: "isFeatured", paramName: "isFeatured")
         ]
     }
 
