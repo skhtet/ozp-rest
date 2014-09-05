cfb7157ef9eacb4ab7680267b549d28f23a25b61
Ross Pokorny <ross.pokorny@nextcentury.com>
2014-09-05 07:24:33 -0600

    OZF-112: Cleaning up unused code

--- src/groovy/marketplace/rest/RepresentationFactory.groovy
+++ src/groovy/marketplace/rest/RepresentationFactory.groovy
@@ -7,19 +7,6 @@ import javax.ws.rs.core.MediaType
 
 interface RepresentationFactory<T> {
     /**
-     * The MediaTypes that go along with the Representation produced by this factory.
-     * Generally this will be one specific media type and optionally one more general one, like
-     * application/json
-     */
-    Set<MediaType> getMediaTypes()
-
-    /**
-     * The Type supported as input to this factory.  Usually this will just be the Class<T>, but
-     * it could be a generic ParameterizedType, especially for collections
-     */
-    Type getSupportedType()
-
-    /**
      * @param sourceObj The object to be transformed into a representation
      * @param uriBuilder a UriBuilder that should be initialized to the application base URI.
      * @return A representation of the sourceObj, appropriate for this factory's MediaType
