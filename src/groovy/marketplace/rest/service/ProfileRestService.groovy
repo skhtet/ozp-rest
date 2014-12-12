4759f659d9473af2a83d1dc8131e7adf8a2dd03a
Ross Pokorny <ross.pokorny@nextcentury.com>
2014-12-12 10:35:09 -0700

    feat(Image Server): Improve login performance

--- src/groovy/marketplace/rest/service/ProfileRestService.groovy
+++ src/groovy/marketplace/rest/service/ProfileRestService.groovy
@@ -8,6 +8,10 @@ import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.annotation.Isolation
 import org.springframework.security.access.AccessDeniedException
 
+import net.sf.ehcache.CacheManager
+import net.sf.ehcache.Cache
+import net.sf.ehcache.Element
+
 import org.codehaus.groovy.grails.commons.GrailsApplication
 
 import marketplace.Profile
@@ -27,10 +31,14 @@ class ProfileRestService extends RestService<Profile> {
     @Autowired AccountService accountService
     @Autowired AgencyRestService agencyRestService
 
+    private final Cache recentLoginsCache
+
     @Autowired
-    public ProfileRestService(GrailsApplication grailsApplication) {
+    public ProfileRestService(GrailsApplication grailsApplication, CacheManager cacheManager) {
         super(grailsApplication, Profile.class, null,
             new Sorter<Profile>(Constants.SortDirection.ASC, 'displayName'))
+
+        this.recentLoginsCache = cacheManager.getCache('recentLogins')
     }
 
     //Keep CGLIB happy
@@ -144,6 +152,15 @@ class ProfileRestService extends RestService<Profile> {
      */
     @Transactional(isolation=Isolation.READ_COMMITTED)
     public void login() {
+        String username = accountService.loggedInUsername
+
+        //this cache is set to expire items after 1 minute, so any
+        //entry in the cache is a recent login
+        if (recentLoginsCache.get(username)) {
+            return
+        }
+
+        //pessimistically lock for update
         Profile profile = getCurrentUserProfile(true) ?: new Profile(
             username: accountService.loggedInUsername
         )
@@ -164,6 +181,7 @@ class ProfileRestService extends RestService<Profile> {
         }
 
         profile.save(failOnError:true)
+        recentLoginsCache.put(new Element(username, null))
     }
 
     @Override
